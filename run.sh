caaa1381a9b85dede701ea49932ed729f0d0c063
2017libin <1963482624@qq.com>
2023-05-14 23:25:28 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+# 第一次运行请运行命令安装相关软件并生成库文件
+# sudo apt-get install autoreconf
+# sudo apt-get install libtool
 # ./autogen.sh && ./configure --with-ecmult-gen-kb=86 && make clean && make -j check &&
 gcc ./src/sm2test.c ./.libs/libsecp256k1.a -o sm2_sign_test -I ./include && ./sm2_sign_test
 gcc ./src/sm2_enc_and_dec_test.c ./.libs/libsecp256k1.a -o sm2_enc_test -I ./include && ./sm2_enc_test
