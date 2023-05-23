8811df355965e042c6a67754cc0ddf37108687d1
Chase51 <1963482624@qq.com>
2023-05-23 23:02:42 +0800

    update sm2 test

--- src/sm2_enc_and_dec_test.c
+++ src/sm2_enc_and_dec_test.c
@@ -16,11 +16,6 @@ static void hex_dump(char prefix[], unsigned char bytes[], size_t bytes_len){
     printf("\n");
 }
 int run(int thread_num){
-    /* Instead of signing the message directly, we must sign a 32-byte hash.
-     * Here the message is "Hello, world!" and the hash function was SHA-256.
-     * An actual implementation should just call SHA-256, but this example
-     * hardcodes the output to avoid depending on an additional library.
-     * See https://bitcoin.stackexchange.com/questions/81115/if-someone-wanted-to-pretend-to-be-satoshi-by-posting-a-fake-signature-to-defrau/81116#81116 */
     unsigned char msg_hash[32] = {
         0x31, 0x5F, 0x5B, 0xDB, 0x76, 0xD0, 0x78, 0xC4,
         0x3B, 0x8A, 0xC0, 0x06, 0x4E, 0x4A, 0x01, 0x64,
@@ -34,7 +29,6 @@ int run(int thread_num){
     unsigned char compressed_pubkey[33];
     unsigned char serialized_signature[64];
     size_t len;
-    int is_signature_valid;
     int return_val;
     secp256k1_pubkey pubkey;
     secp256k1_ecdsa_signature sig;
@@ -48,7 +42,7 @@ int run(int thread_num){
     return_val = secp256k1_context_randomize(ctx, randomize);
     assert(return_val);
 
-    /*** Key Generation ***/
+    // 生成随机私钥
     while (1) {
         if (!fill_random(seckey, sizeof(seckey))) {
             printf("Failed to generate randomness\n");
@@ -58,14 +52,11 @@ int run(int thread_num){
             break;
         }
     }
-
+    // 生成公钥
     return_val = secp256k1_ec_pubkey_create(ctx, &pubkey, seckey);
     assert(return_val);
-    len = sizeof(compressed_pubkey);
-    return_val = secp256k1_ec_pubkey_serialize(ctx, compressed_pubkey, &len, &pubkey, SECP256K1_EC_COMPRESSED);
-    assert(return_val);
-    assert(len == sizeof(compressed_pubkey));
-#if 1
+#if 0
+    // 正确性验证
     secp256k1_sm2_encryption(ctx, msg_hash, sizeof(msg_hash), &pubkey, NULL, NULL, cip);
     hex_dump("明文", msg_hash, 32);
     hex_dump("密文", cip, 128);
@@ -73,23 +64,11 @@ int run(int thread_num){
     hex_dump("解密后", m, 32);
     return 0;
 #endif
-    /*** Enctryption ***/
-//    clock_t start,finish;
-//    double total_time, average_time;
-//    start = clock();
-//    int i = 0;
-//    for(;i < 1;i++){
-//        return_val = secp256k1_sm2_encryption(ctx, msg_hash, sizeof(msg_hash), &pubkey, NULL, NULL, cip);
-//    }
-//    finish = clock();
-//    total_time = (double)(finish - start) / CLOCKS_PER_SEC;
-//    assert(return_val);
-//    printf("encryption %d times, total time %f seconds\n", i,total_time);
-//    printf("average time %f seconds\n", average_time/(float)i);
-//
+#if 1
+    // 测试多线程性能
+    // 加密性能
     double begin,end;
-    size_t count = 1;
-
+    size_t count = 100000;  // 执行次数
     begin = omp_get_wtime();
     #pragma omp parallel for num_threads(thread_num)
     for(int i = 0; i < count;i++){
@@ -99,27 +78,17 @@ int run(int thread_num){
     printf("enc - %d threads: run %d times, , total time: %f s, per second run %f tims\n", \
 			thread_num, count, (end-begin), count/(end-begin));
     assert(return_val);
-    /*** Decryption ***/
-//    start = clock();
-//    for(i = 0;i < 1;i++){
-//        is_signature_valid = secp256k1_sm2_decryption(cip, sizeof(msg_hash), m, seckey);
-//    }
-//    finish = clock();
-//    total_time = (double)(finish - start) / CLOCKS_PER_SEC;
-//    printf("total time %f seconds\n", total_time);
-//    printf("average time %f seconds\n", average_time/i);
+
+    // 解密性能
     begin = omp_get_wtime();
     #pragma omp parallel for num_threads(thread_num)
     for(int i = 0;i < count; i++){
-        is_signature_valid = secp256k1_sm2_decryption(cip, sizeof(msg_hash), m, seckey);
+        secp256k1_sm2_decryption(cip, sizeof(msg_hash), m, seckey);
     }
     end = omp_get_wtime();
     printf("dec - %d threads: run %d times, , total time: %f s, per second run %f tims\n", \
 			thread_num, count, (end-begin), count/(end-begin));
-//
-//   printf("Is the decrytion succeed? %s %d\n", is_signature_valid ? "true" : "false", is_signature_valid);
-//    print_hex(m, 32);
-    /* This will clear everything from the context and free the memory */
+#endif
     secp256k1_context_destroy(ctx);
     memset(seckey, 0, sizeof(seckey));
     return 0;
@@ -127,9 +96,9 @@ int run(int thread_num){
 
 int main(){
     run(1);
-//    run(2);
-//    run(4);
-//    run(8);
-//    run(12);
-//    run(32);
+    run(2);
+    run(4);
+    run(8);
+    run(12);
+    run(32);
 }
